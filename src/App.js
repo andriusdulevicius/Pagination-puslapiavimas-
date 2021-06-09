eda66d7f85ab375d065ca73a9aca2f5dcbfa9ddd
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-09 13:46:43 +0300

    moviesTable table in place

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import MovieTable from './components/movieTable';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container mt-3">
+      {/* <MyHeader /> */}
       <MovieTable />
+      {/* <MovieFooter /> */}
     </div>
   );
 }
--- src/components/movieTable.jsx
+++ src/components/movieTable.jsx
@@ -4,20 +4,37 @@ import { getMovies } from '../services/fakeMovieService';
 class MovieTable extends Component {
   state = {
     movies: getMovies(),
-    count: 1,
   };
 
   render() {
     return (
       <div>
         <h3>Please see out movies</h3>
-        <ol>
-          {this.state.movies.map((m) => (
-            <li key={m._id}>
-              <p>{m.title}</p>
-            </li>
-          ))}
-        </ol>
+        <p>Showing XX movies in out store</p>
+        <table className="table table-striped ">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rating</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button className="btn btn-danger btn-sm">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
@@ -30,5 +47,14 @@ class MovieTable extends Component {
 //https://getbootstrap.com/docs/4.6/content/tables/
 
 // 4 prisideti dar viena stulpeli ir jamae turetu buti delete mygtukas
+
+// 5 prisideti bootstrap ar ne bootsrap headeri kaip atskira komponenta i
+// app.js virs lenteles komponento
+
+// 6 prisideti footeri kaip atsikra komponenta zemiau lenteles
+// footeryje turetu buti siandienos data dinamiskai
+
+// 7 prisideti lorem trumpsum aside su antraste desinene nuo lenteles
+// kaip atskira komponenta
 
 export default MovieTable;
