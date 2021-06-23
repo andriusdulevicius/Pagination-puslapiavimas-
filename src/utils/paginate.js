8795b2a2841c06f87bc88efb3a2ae2967e61e081
AndriusDulevicius <andriusdulevicius@email.com>
2021-06-23 12:38:37 +0300

    sorting movie genres done, proptypes validation done

--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -2,3 +2,7 @@ export function paginate(items, pageNum, pageSize) {
   const startPosition = pageNum * pageSize - pageSize;
   return items.slice(startPosition, startPosition + pageSize);
 }
+
+export function sortGenres(movies, genre) {
+  return movies.filter((movie) => movie.genre.name === genre);
+}
